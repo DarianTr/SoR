e4e42c643b1830ce29181f2e61b0596522811491
ty1phon <109092873+ty1phon@users.noreply.github.com>
2022-12-09 16:24:19 +0100

    pymysql bug fix

--- app.py
+++ app.py
@@ -3,7 +3,9 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_alembic import Alembic
 from sqlalchemy import create_engine
 import config
+import pymysql
 
+pymysql.install_as_MySQLdb()
 engine = create_engine(config.database_url, echo=True)
 engine.connect()
 
